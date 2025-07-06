d6ac47469fac076f0cd1bde0ae343c4bdb832553
lawalthb <lawalthb@gmail.com>
2025-07-06 02:16:19 +0100

    inventory page showing

--- app/Http/Controllers/Tenant/InventoryController.php
+++ app/Http/Controllers/Tenant/InventoryController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Tenant;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tenant;
 use Illuminate\Http\Request;
 
 class InventoryController extends Controller
 {
-    //
+    public function index(Tenant $tenant)
+    {
+        return view('tenant.inventory.index', compact('tenant'));
+    }
 }
